7d0dc27936d329b27d73c3a5cbd29fd37cd2a57c
Koós Zoltán <koos.zoltan.iso@gmail.com>
2021-06-13 12:42:19 +0200

    Added discovery and home view. Added functionality to swap between pages. Added test image. Created bindings between views and viewmodels.

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp.Core;
+
+namespace WpfApp.MVVM.ViewModel
+{
+    public class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+        public HomeViewModel HomeVm { get; set; }
+        public DiscoveryViewModel DiscoveryVm { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; } 
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            DiscoveryVm = new DiscoveryViewModel();
+
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(r => 
+            {
+                CurrentView = HomeVm;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(r =>
+            {
+                CurrentView = DiscoveryVm;
+            });
+        }
+    }
+}
